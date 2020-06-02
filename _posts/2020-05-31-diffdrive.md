46899f96d232c5847fa49068224bbe7c8fbbdb7d
devanshdhrafani <f20171569@hyderabad.bits-pilani.ac.in>
2020-06-02 15:11:55 +0530

    Trying tags and categories

--- _posts/2020-05-31-diffdrive.md
+++ _posts/2020-05-31-diffdrive.md
@@ -1,11 +1,15 @@
 ---
 title: "diff_drive_bot"
 date: 2020-05-31
-tags: [slam, ros]
+tags: 
+  - slam
+  - ros
+  - navigation
+categories:
+  - ROS
 # header:
   #  image: ""
 excerpt: "This ROS package implements SLAM and Autonomous Navigation on a custom 2 wheeled differential drive robot"
 ---
 
 Here is the post. Here we should add content. 
-
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
         "title": "diff_drive_bot",
-        "excerpt":"Here is the post. Here we should add content.   ","categories": [],
-        "tags": ["slam","ros"],
-        "url": "http://localhost:4000/diffdrive/",
+        "excerpt":"Here is the post. Here we should add content.  ","categories": ["ROS"],
+        "tags": ["slam","ros","navigation"],
+        "url": "http://localhost:4000/ros/diffdrive/",
         "teaser": null
       }]
